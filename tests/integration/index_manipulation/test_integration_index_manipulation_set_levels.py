dae8d4676491098f6c45b329e19ba4a602c6d125
Daniel Busch <daniel.busch@climate-resource.com>
2025-05-07 11:29:38 +0200

    more failing tests

--- tests/integration/index_manipulation/test_integration_index_manipulation_set_levels.py
+++ tests/integration/index_manipulation/test_integration_index_manipulation_set_levels.py
@@ -33,9 +33,83 @@ from pandas_openscm.index_manipulation import set_levels
             ),
             id="set-single-level",
         ),
+        pytest.param(
+            pd.MultiIndex.from_tuples(
+                [
+                    ("sa", "va", "kg", 0),
+                    ("sb", "vb", "m", 1),
+                    ("sa", "va", "kg", 2),
+                ],
+                names=["scenario", "variable", "unit", "run_id"],
+            ),
+            {"new_variable": ["a", "b", "c"]},
+            pd.MultiIndex.from_tuples(
+                [
+                    ("sa", "va", "kg", 0, "a"),
+                    ("sb", "vb", "m", 1, "b"),
+                    ("sa", "va", "kg", 2, "c"),
+                ],
+                names=["scenario", "variable", "unit", "run_id", "new_variable"],
+            ),
+            id="set-multiple-levels",
+        ),
+        pytest.param(
+            pd.MultiIndex.from_tuples(
+                [
+                    ("sa", "va", "kg", 0),
+                    ("sb", "vb", "m", 1),
+                    ("sa", "va", "kg", 2),
+                ],
+                names=["scenario", "variable", "unit", "run_id"],
+            ),
+            {"variable": ["a", "b", "c"]},
+            pd.MultiIndex.from_tuples(
+                [
+                    ("sa", "a", "kg", 0),
+                    ("sb", "b", "m", 1),
+                    ("sa", "c", "kg", 2),
+                ],
+                names=["scenario", "variable", "unit", "run_id"],
+            ),
+            id="alter-existing-level",
+        ),
     ),
 )
-def test_update_levels_from_other(start, levels_to_set, exp):
+def test_set_levels(start, levels_to_set, exp):
     res = set_levels(start, levels_to_set=levels_to_set)
 
     pd.testing.assert_index_equal(res, exp)
+
+
+def test_set_levels_raises_type_error():
+    """
+    Test that set_levels raises an error when the levels to set are not in the index.
+    """
+    start = pd.Index(range(10), name="index")
+
+    levels_to_set = {"new_variable": "test"}
+
+    with pytest.raises(TypeError):
+        set_levels(start, levels_to_set=levels_to_set)
+
+
+def test_set_levels_raises_value_error():
+    """
+    Test that set_levels raises an error when the levels to set are same length
+    as index.
+    """
+    start = pd.MultiIndex.from_tuples(
+        [
+            ("sa", "va", "kg", 0),
+            ("sb", "vb", "m", 1),
+            ("sa", "va", "kg", 2),
+        ],
+        names=["scenario", "variable", "unit", "run_id"],
+    )
+
+    levels_to_set = {"new_variable": ["a", "b", "c", "d"]}
+
+    with pytest.raises(
+        ValueError, match="Length of values does not match, got 4 but expected 3"
+    ):
+        set_levels(start, levels_to_set=levels_to_set)
